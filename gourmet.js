d40f39567af6823c3555f6b4d816b44ab352642a
G384382023 <238438@st.takushoku-u.ac.jp>
2025-07-23 19:22:10 +0900

    kadai

--- gourmet.js
+++ gourmet.js
@@ -30,9 +30,9 @@ function printDom(data) {
   b.insertAdjacentElement('beforeend',d);
   for(let r of data.results.shop){
     let p=document.createElement('p');
-    d.insertAdjacentElement('beforeend',p);
-    p.textContent=r+'件目の検索結果';
-    p=document.createElement('p');
+    // d.insertAdjacentElement('beforeend',p);
+    // p.textContent='検索結果'+(r+1)+'件目';
+    // p=document.createElement('p');
     d.insertAdjacentElement('beforeend',p);
     p.textContent='店舗名:'+r.name;
     p=document.createElement('p');
@@ -93,32 +93,68 @@ function printDom(data) {
 }
 
 // 課題6-1 のイベントハンドラ登録処理は以下に記述
-// b = document.querySelector('button');
-// b.addEventListener('click', sendRequest);
+b = document.querySelector('button#print');
+b.addEventListener('click', sendRequest);
 
 
 
 // 課題6-1 のイベントハンドラ sendRequest() の定義
 function sendRequest() {
-  // let url='https://www.nishita-lab.org/web-contents/jsons/hotpepper/{genre}.json';
+  let i=document.querySelector('input[name="genru"]');
+  let k=i.value;
+  let url="https://www.nishita-lab.org/web-contents/jsons/hotpepper/G001.json";
+  if(k==="居酒屋"){
+    url="https://www.nishita-lab.org/web-contents/jsons/hotpepper/G001.json";
+  }else if(k==="ダイニングバー・バル"){
+    url="https://www.nishita-lab.org/web-contents/jsons/hotpepper/G002.json";
+  }else if(k==="創作料理"){
+    url="https://www.nishita-lab.org/web-contents/jsons/hotpepper/G003.json";
+  }else if(k==="和食"){
+    url="https://www.nishita-lab.org/web-contents/jsons/hotpepper/G004.json";
+  }else if(k==="洋食"){
+    url="https://www.nishita-lab.org/web-contents/jsons/hotpepper/G005.json";
+  }else if(k==="イタリアン・フレンチ"){
+    url="https://www.nishita-lab.org/web-contents/jsons/hotpepper/G006.json";
+  }else if(k==="中華"){
+    url="https://www.nishita-lab.org/web-contents/jsons/hotpepper/G007.json";
+  }else if(k==="焼肉・ホルモン"){
+    url="https://www.nishita-lab.org/web-contents/jsons/hotpepper/G008.json";
+  }else if(k==="アジア・エスニック料理"){
+    url="https://www.nishita-lab.org/web-contents/jsons/hotpepper/G009.json";
+  }else if(k==="各国料理"){
+    url="https://www.nishita-lab.org/web-contents/jsons/hotpepper/G010.json";
+  }else if(k==="カラオケ・パーティ"){
+    url="https://www.nishita-lab.org/web-contents/jsons/hotpepper/G011.json";
+  }else if(k==="バー・カクテル"){
+    url="https://www.nishita-lab.org/web-contents/jsons/hotpepper/G012.json";
+  }else if(k==="ラーメン"){
+    url="https://www.nishita-lab.org/web-contents/jsons/hotpepper/G013.json";
+  }else if(k==="カフェ・スイーツ"){
+    url="https://www.nishita-lab.org/web-contents/jsons/hotpepper/G014.json";
+  }else if(k==="その他のグルメ"){
+    url="https://www.nishita-lab.org/web-contents/jsons/hotpepper/G015.json";
+  }else if(k==="お好み焼き・もんじゃ"){
+    url="https://www.nishita-lab.org/web-contents/jsons/hotpepper/G016.json";
+  }else if(k==="韓国料理"){
+    url="https://www.nishita-lab.org/web-contents/jsons/hotpepper/G017.json";
+  }
+  
 
-  // axios.get(url)
-  // .then(showResult)//成功
-  // .catch(showError)//失敗
-  // .then(finish);//最後の処理
+  axios.get(url)
+  .then(showResult)//成功
+  .catch(showError)//失敗
+  .then(finish);//最後の処理
 }
 
 // 課題6-1: 通信が成功した時の処理は以下に記述
 function showResult(resp) {
-  // //データを出力
-  // let data=resp.data;
-  // //dataが文字列型ならオブジェクトに変換
-  // if(typeof data==='string'){
-  //   data=JSON.parse(data);
-  // }
-  // //したの２ぎょうを変える
-  // // console.log(data);
-  // // console.log(data.x);
+  //データを出力
+  let data=resp.data;
+  //dataが文字列型ならオブジェクトに変換
+  if(typeof data==='string'){
+    data=JSON.parse(data);
+  }
+  printDom(data);
 }
 
 // 課題6-1: 通信エラーが発生した時の処理
@@ -135,204 +171,3 @@ function finish() {
 // 以下はグルメのデータサンプル
 // 注意: 第5回までは以下を変更しないこと！
 // 注意2: 課題6-1 で以下をすべて削除すること
-let data = {
-  "results": {
-    "api_version": "1.26",
-    "results_available": 52,
-    "results_returned": "10",
-    "results_start": 1,
-    "shop": [
-      {
-        "access": "京王八王子駅を背にし右手に見えるローソンの隣のビル、ホテルザ・ビーの2階です。",
-        "address": "東京都八王子市明神町４-6-12　ホテル・ザ・ビー八王子（旧八王子プラザホテル）2F",
-        "band": "可",
-        "barrier_free": "なし",
-        "budget": {
-          "average": "ディナー3000円",
-          "code": "B003",
-          "name": "3001～4000円"
-        },
-        "budget_memo": "",
-        "capacity": 100,
-        "card": "利用可",
-        "catch": "【サプライズ演出有】 結婚パーティー受付中",
-        "charter": "貸切可 ：VIPルーム1部屋2～15名様前後（完全個室）/パーティーフロア！50型モニター・スポットライト等充実",
-        "child": "お子様連れOK",
-        "close": "不定休有り。店休時は店長携帯電話09035324825まで問い合わせください♪",
-        "coupon_urls": {
-          "pc": "https://www.hotpepper.jp/strJ000989843/map/?vos=nhppalsa000016",
-          "sp": "https://www.hotpepper.jp/strJ000989843/scoupon/?vos=nhppalsa000016"
-        },
-        "course": "あり",
-        "english": "なし",
-        "free_drink": "あり",
-        "free_food": "なし",
-        "genre": {
-          "catch": "大人の社交場非日常を味わうダイニング",
-          "code": "G001",
-          "name": "居酒屋"
-        },
-        "horigotatsu": "なし",
-        "id": "J000989843",
-        "karaoke": "あり",
-        "ktai_coupon": 0,
-        "large_area": {
-          "code": "Z011",
-          "name": "東京"
-        },
-        "large_service_area": {
-          "code": "SS10",
-          "name": "関東"
-        },
-        "lat": 35.6585460152,
-        "lng": 139.34327231,
-        "logo_image": "https://imgfp.hotp.jp/IMGH/21/04/P038512104/P038512104_69.jpg",
-        "lunch": "なし",
-        "middle_area": {
-          "code": "Y110",
-          "name": "八王子・立川"
-        },
-        "midnight": "営業していない",
-        "mobile_access": "京王八王子駅1分/JR八王子駅北口5分",
-        "name": "バグダッドカフェ Bagdadcafe/モータウン MOTOWN",
-        "name_kana": "ばぐだっどかふぇもーたうんはちおうじ",
-        "non_smoking": "禁煙席なし",
-        "open": "月～日、祝日、祝前日: 17:00～21:00 （料理L.O. 20:00 ドリンクL.O. 20:00）",
-        "other_memo": "ステージ・マイク・カラオケ等",
-        "parking": "なし",
-        "party_capacity": 250,
-        "pet": "不可",
-        "photo": {
-          "mobile": {
-            "l": "https://imgfp.hotp.jp/IMGH/22/35/P038512235/P038512235_168.jpg",
-            "s": "https://imgfp.hotp.jp/IMGH/22/35/P038512235/P038512235_100.jpg"
-          },
-          "pc": {
-            "l": "https://imgfp.hotp.jp/IMGH/22/35/P038512235/P038512235_238.jpg",
-            "m": "https://imgfp.hotp.jp/IMGH/22/35/P038512235/P038512235_168.jpg",
-            "s": "https://imgfp.hotp.jp/IMGH/22/35/P038512235/P038512235_58_s.jpg"
-          }
-        },
-        "private_room": "あり ：VIPルーム1部屋2～15名様前後（完全個室）/パーティーフロア",
-        "service_area": {
-          "code": "SA11",
-          "name": "東京"
-        },
-        "shop_detail_memo": "ご質問、ご要望はお気軽にお問い合わせください♪",
-        "show": "あり",
-        "small_area": {
-          "code": "X220",
-          "name": "八王子"
-        },
-        "station_name": "京王八王子",
-        "sub_genre": {
-          "code": "G002",
-          "name": "ダイニングバー・バル"
-        },
-        "tatami": "なし",
-        "tv": "あり",
-        "urls": {
-          "pc": "https://www.hotpepper.jp/strJ000989843/?vos=nhppalsa000016"
-        },
-        "wedding": "大歓迎！ステージ・マイク・音響・映像等、設備充実！！最大200名様まで是非どうぞ★",
-        "wifi": "あり"
-      },
-      {
-        "access": "ＪＲ八王子駅北口徒歩1分/京王八王子駅徒歩3分",
-        "address": "東京都八王子市東町12-14",
-        "band": "不可",
-        "barrier_free": "あり ：スタッフがお手伝いさせて頂きます。ご不明な点等お気軽にお声掛け下さい。事前のお問い合わせも◎",
-        "budget": {
-          "average": "2,000円(通常平均) 3,000円(宴会平均) ",
-          "code": "B002",
-          "name": "2001～3000円"
-        },
-        "budget_memo": "",
-        "capacity": 100,
-        "card": "利用可",
-        "catch": "2.5時間飲み放題付2000円~ 【★少人数様ソファー★】",
-        "charter": "貸切可 ：八王子で人気の個室バル最大50名様まで対応できます。20名様～フロア貸切応相談　最大100名様までOK",
-        "child": "お子様連れ歓迎 ：チャイルドシートもご用意しています。お子様連れのママ会・ご家族でのご利用にもお気軽にどうぞ♪",
-        "close": "【年中無休】貸切宴会のご予約も絶賛受付中！最大100名様までご利用可能ですので、会社宴会 同窓会 宴会にオススメ♪",
-        "coupon_urls": {
-          "pc": "https://www.hotpepper.jp/strJ001275177/map/?vos=nhppalsa000016",
-          "sp": "https://www.hotpepper.jp/strJ001275177/scoupon/?vos=nhppalsa000016"
-        },
-        "course": "あり",
-        "english": "なし",
-        "free_drink": "あり ：★NEW OPEN★当店イチ押しのコース★2.5h飲み放題付き「Funny Classicコース」4980円→2980円",
-        "free_food": "あり ：★NEW OPEN★当店イチ押しのコース★2.5h飲み放題付き「Funny Classicコース」4980円→2980円",
-        "genre": {
-          "catch": "八王子 個室居酒屋 飲み放題 肉バル 女子会",
-          "code": "G001",
-          "name": "居酒屋"
-        },
-        "horigotatsu": "なし ：温かな照明と開放的な店内で楽しくご宴会♪八王子での宴会 飲み会 女子会 二次会 合コンに◎",
-        "id": "J001275177",
-        "karaoke": "なし",
-        "ktai_coupon": 0,
-        "large_area": {
-          "code": "Z011",
-          "name": "東京"
-        },
-        "large_service_area": {
-          "code": "SS10",
-          "name": "関東"
-        },
-        "lat": 35.6582259169,
-        "lng": 139.3381858122,
-        "logo_image": "https://imgfp.hotp.jp/IMGH/61/98/P038366198/P038366198_69.jpg",
-        "lunch": "なし",
-        "middle_area": {
-          "code": "Y110",
-          "name": "八王子・立川"
-        },
-        "midnight": "営業している",
-        "mobile_access": "JR八王子駅北口徒歩1分/京王八王子駅徒歩3分",
-        "name": "隠れ家バル Funny&Bouquet",
-        "name_kana": "かくれやばる　ふぁにーあんどぶーけ",
-        "non_smoking": "禁煙席なし",
-        "open": "月～日、祝日、祝前日: 17:00～翌5:00 （料理L.O. 翌3:00 ドリンクL.O. 翌4:00）",
-        "other_memo": "個室や貸切の詳細等お気軽にお電話にてお問い合わせください。八王子での飲み会に◎",
-        "parking": "なし ：お近くのコインパーキングをご利用下さい。",
-        "party_capacity": 50,
-        "pet": "不可",
-        "photo": {
-          "mobile": {
-            "l": "https://imgfp.hotp.jp/IMGH/33/59/P038703359/P038703359_168.jpg",
-            "s": "https://imgfp.hotp.jp/IMGH/33/59/P038703359/P038703359_100.jpg"
-          },
-          "pc": {
-            "l": "https://imgfp.hotp.jp/IMGH/33/59/P038703359/P038703359_238.jpg",
-            "m": "https://imgfp.hotp.jp/IMGH/33/59/P038703359/P038703359_168.jpg",
-            "s": "https://imgfp.hotp.jp/IMGH/33/59/P038703359/P038703359_58_s.jpg"
-          }
-        },
-        "private_room": "なし ：個室　2名×4室　4名×8室　8名～10名×4室　20名～40名×2室",
-        "service_area": {
-          "code": "SA11",
-          "name": "東京"
-        },
-        "shop_detail_memo": "★NEW OPEN★当店イチ押しのコース★2.5h飲み放題付き「Funny Classicコース」4980円→2980円",
-        "show": "なし",
-        "small_area": {
-          "code": "X220",
-          "name": "八王子"
-        },
-        "station_name": "八王子",
-        "sub_genre": {
-          "code": "G002",
-          "name": "ダイニングバー・バル"
-        },
-        "tatami": "なし ：八王子で人気のお座敷個室！最大50名様まで対応できます。",
-        "tv": "あり",
-        "urls": {
-          "pc": "https://www.hotpepper.jp/strJ001275177/?vos=nhppalsa000016"
-        },
-        "wedding": "八王子での結婚式の二次会や同窓会、打ち上げパーティー等多様なシーンにご利用下さい",
-        "wifi": "あり"
-      }
-    ]
-  }
-};
-
